26ac2984414d1e2d8bf2c72fb3e8ac4929132e74
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-02-03 15:28:23 -0500

    Initial commit

---
+++ blackjack_advisor.rb
@@ -0,0 +1,20 @@
+
+def player_hand
+  puts "How many points do you have in your hand?"
+  gets.chomp
+end
+
+def dealer_hand
+  puts "How many points does the dealer have faceup?"
+  gets.chomp
+end
+
+def ace
+  puts "Do you have an ace?"
+  gets.chomp
+end
+
+def pairs
+  puts "Do you have pairs?"
+  gets.chomp
+end
