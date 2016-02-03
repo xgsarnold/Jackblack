cd6c6e823b0b2a4b236526f6488be434f1d20b6c
Geoffrey Arnold <geoffrey.s.arnold@gmail.com>
2016-02-03 17:49:20 -0500

    replace face_card? and ace? methods with single card_convert method to run all user inputs through.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -14,15 +14,15 @@ def dealer_card?
   gets.chomp
 end
 
-#def ace
-#  puts "Do you have an ace?"
-#  gets.chomp
-#end
-#
-#def pairs
-#  puts "Do you have pairs?"
-#  gets.chomp
-#end
+def card_convert(card)
+  if card == "j" || card == "q" || card == "k"
+    10
+  elsif card == "a"
+    11
+  else
+    card
+  end
+end
 
 hard = Hash.new()
 hard[5] = Hash.new("Hit!")
@@ -39,7 +39,7 @@ hard[9][5] = "Double Down!"
 hard[9][6] = "Double Down!"
 hard[10] = Hash.new("Double Down!")
 hard[10][10] = "Hit!"
-hard[10]["a"] = "Hit!"
+hard[10][11] = "Hit!"
 hard[11] = Hash.new("Double Down!")
 hard[12] = Hash.new("Hit!")
 hard[12][4] = "Stay ..."
@@ -62,13 +62,13 @@ hard[15][7] = "Hit!"
 hard[15][8] = "Hit!"
 hard[15][9] = "Hit!"
 hard[15][10] = "Hit!"
-hard[15]["a"] = "Hit!"
+hard[15][11] = "Hit!"
 hard[16] = Hash.new("Stay ...")
 hard[16][7] = "Hit!"
 hard[16][8] = "Hit!"
 hard[16][9] = "Hit!"
 hard[16][10] = "Hit!"
-hard[16]["a"] = "Hit!"
+hard[16][11] = "Hit!"
 
 soft = Hash.new()
 soft[13] = Hash.new("Hit!")
@@ -110,46 +110,38 @@ pair[2] = Hash.new("Split!")
 pair[2][8] = "Hit!"
 pair[2][9] = "Hit!"
 pair[2][10] = "Hit!"
-pair[2]["a"] = "Hit!"
+pair[2][11] = "Hit!"
 pair[3] = Hash.new()
 pair[3][9] = "Hit!"
 pair[3][10] = "Hit!"
-pair[3]["a"] = "Hit!"
+pair[3][11] = "Hit!"
 pair[4] = Hash.new("Hit!")
 pair[4][4] = "Split!"
 pair[4][5] = "Split!"
 pair[4][6] = "Split!"
 pair[5] = Hash.new("Double Down!")
 pair[5][10] = "Hit!"
-pair[5]["a"] = "Hit!"
+pair[5][11] = "Hit!"
 pair[6] = Hash.new("Split!")
 pair[6][8] = "Hit!"
 pair[6][9] = "Hit!"
 pair[6][10] = "Hit!"
-pair[6]["a"] = "Hit!"
+pair[6][11] = "Hit!"
 pair[7] = Hash.new("Split!")
 pair[7][9] = "Hit!"
 pair[7][10] = "Stand ..."
-pair[7]["a"] = "Hit!"
+pair[7][11] = "Hit!"
 pair[8] = Hash.new("Split!")
 pair[9] = Hash.new("Split!")
 pair[9][7] = "Stand ..."
 pair[9][9] = "Stand ..."
 pair[9][10] = "Stand ..."
 pair[10] = Hash.new("Stand ...")
-pair["a"] = Hash.new("Split!")
+pair[11] = Hash.new("Split!")
 
-card_1 = card_1?
-card_2 = card_2?
-dealer_card = dealer_card?
-
-if card_1 == "a"
-  card_1 = 11
-elsif card_2 == "a"
-  card_2 = 11
-elsif dealer_card == "a"
-  dealer_card = 11
-end
+card_1 = card_convert(card_1?.downcase)
+card_2 = card_convert(card_2?.downcase)
+dealer_card = card_convert(dealer_card?.downcase)
 
 if card_1 == card_2
   puts "You should #{pair[card_1.to_i][dealer_card.to_i]}"
@@ -158,5 +150,3 @@ elsif card_1 != 11 && card_2 != 11
 elsif card_1 == 11 || card_2 == 11
   puts "You should #{soft[(card_1.to_i + card_2.to_i)][dealer_card.to_i]}"
 end
-
-#if card_1 == "a" || card_2 == "a"
